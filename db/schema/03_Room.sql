43a5369d540b4439adc152b21d44387bea0df6f1
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-07-30 12:54:37 +0300

    Rename 'RoomStatus' column to 'IsAvailable' in Room table

--- db/schema/03_Room.sql
+++ db/schema/03_Room.sql
@@ -5,8 +5,8 @@ CREATE TABLE Room(
     RoomName VARCHAR(50) UNIQUE NOT NULL,
     RoomLocation VARCHAR(100) NOT NULL,
     RoomCapacity TINYINT NOT NULL,
-    RoomStatus BIT NOT NULL DEFAULT 1, -- 1 = Available, 0 = Not Available
+    IsAvailable BIT NOT NULL DEFAULT 1,
     PRIMARY KEY(RoomId),
     CONSTRAINT CHK_RoomCapacity CHECK (RoomCapacity > 0),
-    CONSTRAINT CHK_RoomStatus CHECK (RoomStatus IN (0, 1))
+    CONSTRAINT CHK_RoomStatus CHECK (IsAvailable IN (0, 1))
 );
