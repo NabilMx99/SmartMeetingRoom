63d7861d5e4455fd098e3d983d38503dfae7c08d
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-07-31 23:12:38 +0300

    Add entry point 'Program.cs' for Smart Meeting Room API application

---
+++ api/SmartMeetingRoom.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using SmartMeetingRoom.API.Data;
+
+namespace SmartMeetingRoom.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<SmartMeetingRoomDBContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("SmartMeetingRoomDB")));
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
