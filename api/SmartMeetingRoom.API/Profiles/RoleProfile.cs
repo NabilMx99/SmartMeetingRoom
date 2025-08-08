8bf6c694cf36be4f39aaa0da83d384fbaa8815f3
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-08 18:36:13 +0300

    Add RoleProfile class with custom mapping using AutoMapper

---
+++ api/SmartMeetingRoom.API/Profiles/RoleProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SmartMeetingRoom.API.Models;
+using SmartMeetingRoom.API.DTOs;
+
+namespace SmartMeetingRoom.API.Profiles;
+
+public class RoleProfile : Profile
+{
+    public RoleProfile()
+    {
+        CreateMap<Role, RoleDto>()
+            .ForMember(dest => dest.RoleDescription, opt =>
+             opt.MapFrom(src => string.IsNullOrEmpty(src.RoleDescription) ? "No description provided." : src.RoleDescription));
+
+        CreateMap<RoleCreateDto, Role>()
+            .ForMember(dest => dest.RoleDescription, opt =>
+            opt.MapFrom(src => string.IsNullOrEmpty(src.RoleDescription) ? "No description provided." : src.RoleDescription));
+
+        CreateMap<RoleUpdateDto, Role>()
+            .ForMember(dest => dest.RoleDescription, opt =>
+            opt.MapFrom(src => string.IsNullOrEmpty(src.RoleDescription) ? "No description provided." : src.RoleDescription));
+    }
+}
