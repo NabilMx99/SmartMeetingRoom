931df21a185a8e9405bbbeba1141fd41f4646f83
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-12 16:22:17 +0300

    Add UserProfile AutoMapper configuration for ApplicationUser model and related DTO mappings

---
+++ api/SmartMeetingRoom.API/Profiles/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SmartMeetingRoom.API.DTOs.User;
+using SmartMeetingRoom.API.DTOs.Role;
+using SmartMeetingRoom.API.Models;
+
+namespace SmartMeetingRoom.API.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<ApplicationUser, UserDto>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.FkRole))
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+
+        CreateMap<UserProfileDto, ApplicationUser>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.UserName, opt => opt.Ignore())
+            .ForMember(dest => dest.EmailConfirmed, opt => opt.Ignore())
+            .ForMember(dest => dest.NormalizedEmail, opt => opt.Ignore())
+            .ForMember(dest => dest.NormalizedUserName, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.SecurityStamp, opt => opt.Ignore())
+            .ForMember(dest => dest.ConcurrencyStamp, opt => opt.Ignore());
+
+        CreateMap<ApplicationRole, RoleDto>();
+    }
+}
+
