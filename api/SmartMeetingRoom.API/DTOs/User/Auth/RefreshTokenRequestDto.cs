1f705339d578a9b95220a0a43f07519f5ce8c3bd
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-12 15:25:13 +0300

    Add RefreshTokenRequestDto and RefreshTokenResponseDto for token refresh functionality

---
+++ api/SmartMeetingRoom.API/DTOs/User/Auth/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartMeetingRoom.API.DTOs.User.Auth;
+
+public class RefreshTokenRequestDto
+{
+    public string Token { get; set; } = null!;
+
+    public string IpAddress { get; set; } = null!;
+}
---
+++ api/SmartMeetingRoom.API/DTOs/User/Auth/RefreshTokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartMeetingRoom.API.DTOs.User.Auth;
+
+public class RefreshTokenResponseDto
+{
+    public string Token { get; set; } = null!;
+
+    public string RefreshToken { get; set; } = null!;
+}
