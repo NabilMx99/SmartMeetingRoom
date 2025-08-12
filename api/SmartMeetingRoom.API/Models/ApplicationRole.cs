6c4545189221bb021d6efc2a04ce0ec0e27432b5
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-12 15:32:18 +0300

    Add custom ApplicationUser and ApplicationRole models based on Identity

---
+++ api/SmartMeetingRoom.API/Models/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SmartMeetingRoom.API.Models;
+
+public class ApplicationRole : IdentityRole<int>
+{
+    public string? RoleDescription { get; set; }
+
+    public virtual ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+}
---
+++ api/SmartMeetingRoom.API/Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SmartMeetingRoom.API.Models;
+
+public class ApplicationUser : IdentityUser<int>
+{
+    public int FkRoleId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public bool IsOnline { get; set; }
+
+    public virtual ApplicationRole FkRole { get; set; } = null!;
+
+    public virtual ICollection<Meeting> Meetings { get; set; } = new List<Meeting>();
+
+    public virtual ICollection<Attendee> Attendees { get; set; } = new List<Attendee>();
+
+    public virtual ICollection<MeetingMinute> MeetingMinutes { get; set; } = new List<MeetingMinute>();
+
+    public virtual ICollection<ActionItem> ActionItems { get; set; } = new List<ActionItem>();
+
+    public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+}
