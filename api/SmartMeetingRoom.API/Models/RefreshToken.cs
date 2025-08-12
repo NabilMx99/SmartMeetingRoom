f5048dfef3b8ef4792618c140f63a613c5fa10f2
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-12 15:38:04 +0300

    Add RefreshToken model to support JWT token refresh functionality

---
+++ api/SmartMeetingRoom.API/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartMeetingRoom.API.Models;
+
+public class RefreshToken
+{
+    public int RefreshTokenId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime Created { get; set; }
+
+    public DateTime Expires { get; set; }
+
+    public bool IsRevoked { get; set; }
+
+    public string CreatedByIp { get; set; } = null!;
+
+    public int ApplicationUserId { get; set; }
+
+    public virtual ApplicationUser User { get; set; } = null!;
+}
