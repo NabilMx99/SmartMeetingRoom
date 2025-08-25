61379a3b30838db1b19b3bc726cd94cb09c57c94
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-25 21:41:38 +0300

    Add FeatureProfile class with custom mapping using AutoMapper

---
+++ api/SmartMeetingRoom.API/Profiles/FeatureProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SmartMeetingRoom.API.DTOs.Feature;
+using SmartMeetingRoom.API.Models;
+
+namespace SmartMeetingRoom.API.Profiles;
+
+public class FeatureProfile : Profile
+{
+    public FeatureProfile()
+    {
+        CreateMap<Feature, FeatureDto>();
+        
+        CreateMap<FeatureCreateDto, Feature>();
+
+        CreateMap<FeatureUpdateDto, Feature>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
