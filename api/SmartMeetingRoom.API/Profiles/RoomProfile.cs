c01fc8f2db56529ccf74496435421edb14ff2282
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-08-25 21:20:41 +0300

    Add RoomProfile class with custom mapping using AutoMapper

---
+++ api/SmartMeetingRoom.API/Profiles/RoomProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SmartMeetingRoom.API.DTOs.Feature;
+using SmartMeetingRoom.API.DTOs.Room;
+using SmartMeetingRoom.API.Models;
+
+namespace SmartMeetingRoom.API.Profiles;
+
+public class RoomProfile : Profile
+{
+    public RoomProfile()
+    {
+        CreateMap<Room, RoomDto>()
+            .ForMember(dest => dest.Features, opt => opt.MapFrom(src => src.RoomFeatures.Select(rf => rf.FkFeature)));
+
+        CreateMap<RoomCreateDto, Room>();
+
+        CreateMap<RoomUpdateDto, Room>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+        CreateMap<Feature, FeatureDto>();
+    }
+}
