0d7780d0f76de85ee36c2a83a4e194f9847b2976
Nabil Al Masri <NabilMx99@users.noreply.github.com>
2025-09-05 15:17:42 +0300

    Add AttendeeProfile class with custom mapping using AutoMapper

---
+++ api/SmartMeetingRoom.API/Profiles/AttendeeProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+using SmartMeetingRoom.API.DTOs.Attendee;
+using SmartMeetingRoom.API.Models;
+
+namespace SmartMeetingRoom.API.Profiles
+{
+    public class AttendeeProfile : Profile
+    {
+        public AttendeeProfile()
+        {
+            CreateMap<Attendee, AttendeeDto>()
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.FkUser))
+                .ForMember(dest => dest.AttendeeId, opt => opt.MapFrom(src => src.AttendeeId))
+                .ForMember(dest => dest.AttendeeStatus, opt => opt.MapFrom(src => src.AttendeeStatus));
+
+            CreateMap<AttendeeUpdateDto, Attendee>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        }
+    }
+}
